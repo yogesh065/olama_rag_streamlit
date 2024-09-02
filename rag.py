6d0526b0ca212750cf980577b0e898f4e2569741
Yogesh Mane <yogeshmane@Yogeshs-MacBook-Pro.local>
2024-09-02 23:54:13 +0530

    r

--- rag.py
+++ rag.py
@@ -50,8 +50,8 @@ class ChatPDF:
         self.retriever = vector_store.as_retriever(
             search_type="similarity_score_threshold",
             search_kwargs={
-                "k": 3,
-                "score_threshold": 0.5,
+                "k": 20,
+                "score_threshold": 0.3,
             },
         )
         st.write("embedding done")
